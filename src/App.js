7a6ecd524400cc90e1627706856c023baae7e706
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-05-21 10:46:58 +0200

    commit react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GenerateQuote from './GenerateQuote/index';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GenerateQuote name={GenerateQuote} />
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DisplayQuote/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DisplayQuote=({ item })=>{
+	return (
+		<div className="DisplayQuote">
+		    <ul>
+	        	<li>character : {item.character}</li>
+		    	<li><img src={item.image} alt="ok" /></li>
+		        <li>quote : {item.quote}</li>
+		    </ul>
+		</div>
+	)
+};
+
+export default DisplayQuote;
+
+
